434699ca7e70a0ee5c46627d98cee53852523583
Jian yong Lei <jianyonglei@gmail.com>
2022-11-30 23:19:29 -0500

    addeding routes for users and notes

---
+++ src/features/auth/Welcome.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const Welcome = () => {
+    const date = new Date()
+    const today = new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(date)
+
+  return (
+    <section className="welcome">
+        <p>{today}</p>
+
+        <h1>Welcome!</h1>
+
+        <p><Link to="/dash/notes">View Notes</Link></p>
+
+        <p><Link to="/dash/users">View User Settings</Link></p>
+    </section>
+  )
+}
+
+export default Welcome
