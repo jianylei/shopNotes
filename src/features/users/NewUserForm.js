2f70886ab74c2545172dd4895c46a2fcf7751597
Jian yong Lei <jianyonglei@gmail.com>
2022-12-01 21:11:46 -0500

    added setupListeners to store.js

--- src/features/users/NewUserForm.js
+++ src/features/users/NewUserForm.js
@@ -37,7 +37,7 @@ const NewUserForm = () => {
             setUsername('')
             setPassword('')
             setRoles([])
-            navigate('/dash/user')
+            navigate('/dash/users')
         }
     }, [isSuccess, navigate])
 
