9b6d180210f33dc9af2e34a8f285e3f7029fa7b9
Jian yong Lei <jianyonglei@gmail.com>
2022-12-01 21:03:28 -0500

    added Prefetch to /dash routes

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import EditUsers from './features/users/EditUsers';
 import NewUserForm from './features/users/NewUserForm';
 import EditNote from './features/notes/EditNote';
 import NewNote from './features/notes/NewNote';
+import Prefetch from './features/auth/Prefetch';
 
 function App() {
   return (
@@ -18,21 +19,21 @@ function App() {
         <Route index element={<Public />} />
         <Route path='login' element={<Login />} />
 
-        <Route path='dash' element={<DashLayout />}>
-          <Route index element={<Welcome />} />
-
-          <Route path='users'>
-            <Route index element={<UsersList />} />
-            <Route path=":id" element={<EditUsers />}></Route>
-            <Route path="new" element={<NewUserForm />}></Route>
-          </Route>
-
-          <Route path='notes'>
-            <Route index element={<NotesList />} />
-            <Route path=":id" element={<EditNote />}></Route>
-            <Route path="new" element={<NewNote />}></Route>
-          </Route>
-        </Route> {/* End Dash */}
+        <Route element={<Prefetch />}>
+          <Route path='dash' element={<DashLayout />}>
+            <Route index element={<Welcome />} />
+            <Route path='users'>
+              <Route index element={<UsersList />} />
+              <Route path=":id" element={<EditUsers />}></Route>
+              <Route path="new" element={<NewUserForm />}></Route>
+            </Route>
+            <Route path='notes'>
+              <Route index element={<NotesList />} />
+              <Route path=":id" element={<EditNote />}></Route>
+              <Route path="new" element={<NewNote />}></Route>
+            </Route>
+          </Route> {/* End Dash */}
+        </Route>
       </Route>
     </Routes>
   );
