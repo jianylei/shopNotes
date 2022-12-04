48bcb10dd1fc0ca5aa61cfea977d1042dfb8311f
Jian yong Lei <jianyonglei@gmail.com>
2022-12-04 02:07:40 -0500

    updated apiSlice.js baseUrl

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://jyl-notes-api.onrender.com',
+    baseUrl: 'https://dark-red-elk-wrap.cyclic.app',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
