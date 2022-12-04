5d3394f1078c489f87bef87b378e13fad12e0c19
Jian yong Lei <jianyonglei@gmail.com>
2022-12-03 19:55:09 -0500

    ready for deployment

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:3030',
+    baseUrl: 'https://jyl-notes-api.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
