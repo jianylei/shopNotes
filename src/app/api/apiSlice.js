e8a75cd60bfe605fb8ff74ebce86c8737448fbba
Jian yong Lei <jianyonglei@gmail.com>
2022-12-06 22:25:47 -0500

    fixed apiSlice.js baseUrl

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,8 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    //baseUrl: 'https://dark-red-elk-wrap.cyclic.app',
-    baseUrl: 'http://localhost:3030',
+    baseUrl: 'https://dark-red-elk-wrap.cyclic.app',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
